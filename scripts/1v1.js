28e327cf50131daad7ed6a422232643fbe927feb
Thier Richard Istvan <u9vata@gmail.com>
2016-01-25 19:06:34 +0100

    Initial commit - basic html template of the 1v1 page and sprite rendering of hand - pubnub communication is added

---
+++ scripts/1v1.js
@@ -0,0 +1,87 @@
+var cardWidth = 59;
+var cardHeigth = 92;
+
+// Split the GET parameters into an assoc array
+function transformToAssocArray( prmstr ) {
+    var params = {};
+    var prmarr = prmstr.split("&");
+    for ( var i = 0; i < prmarr.length; i++) {
+	var tmparr = prmarr[i].split("=");
+	params[tmparr[0]] = tmparr[1];
+    }
+    return params;
+}
+
+// Helper function to get the search parameters
+function getSearchParameters() {
+      var prmstr = window.location.search.substr(1);
+      return prmstr != null && prmstr != "" ? transformToAssocArray(prmstr) : {};
+}
+
+// Get the 'GET' parameters - we need the player and the enemy
+var params = getSearchParameters();
+console.log("player: " + params.player);
+console.log("enemy: " + params.enemy);
+
+// Create the name of the channel: This should be unique to players but the same on both sides...
+var player1 = params.player < params.enemy ? params.player : params.enemy;
+var player2 = params.player > params.enemy ? params.player : params.enemy;
+var chName = "greasefat_" + player1 + "_" + player2;
+console.log("channel: " + chName);
+
+// Holds the current hand of the user (TODO: just prefilled with bogus data to see the structure)
+var hand = [{value:7, color:0}, {value:11, color:1}, {value:11, color:3}, {value:14, color:2}]
+
+// Initialize the PUBNUB communication
+var gametopic = PUBNUB.init({
+	publish_key: 'pub-c-6036e039-6cec-46af-8961-389cc5ca156d',
+	subscribe_key: 'sub-c-f5954296-c1c0-11e5-b684-02ee2ddab7fe'
+});
+
+
+// Called in the onLoad event of the body of the 1v1.html
+function loaded(){
+
+	// Subscribing to the game topic (communication channel)
+	gametopic.subscribe({
+		channel: chName,
+		message: onMessage,
+		connect: join
+	});
+
+	// Notification about joining the game called when connection is established
+	function join() {
+		// Other player should see that I have joined the game...
+		publish({cmd:"join", who:params.player});
+	}
+}
+
+// Helper function to publish messages on the channel
+function publish(msg) {
+	gametopic.publish({
+		channel: chName,
+		message: msg
+	});
+}
+
+// The main onMessage event handler function for the game topic
+function onMessage(msg) {
+	// TODO: Handle messages according to the protocol of the game!
+	console.log(msg);
+}
+
+// This function is used to update the hand of the player:
+// - The hand array will be used to change css sprites!
+function updateHand(){
+	$("div#hand div.card").each(function(index){
+		var card = hand[index];
+		var y = card.color * cardHeigth;
+		var x = (14 - card.value) * cardWidth;
+		var pos = -x + "px " + y + "px";
+		console.log(pos);
+		// TODO: update the card
+		$(this).css({
+			"background-position":pos,
+		});
+	});
+}
